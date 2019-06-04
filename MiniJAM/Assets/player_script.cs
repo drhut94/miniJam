360c82453abfe17d963aa47435c7590bbc7f0361
Jan Jongejeugd Serrat <drhut94@gmail.com>
2019-06-04 14:37:54 +0200

    Hola

---
+++ MiniJAM/Assets/player_script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player_script : MonoBehaviour {
+
+    public GameObject image;
+    public GameObject image2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            reloadScene();
+        }
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Lava")) {
+            image2.SetActive(true);
+            Invoke("reloadScene", 3);
+        }
+
+        if (other.gameObject.CompareTag("examen")) {
+            image.gameObject.SetActive(true);
+        }
+    }
+
+    public void reloadScene() {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
